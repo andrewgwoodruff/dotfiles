ff9ec629a23bfb3b5806a2ca951f1aa61469d748
Andrew Woodruff <agwoodruff93@gmail.com>
2019-10-18 18:24:03 -0600

    adding my .gitconfig and .gitignore files

--- scripts/install-dotfiles.sh
+++ scripts/install-dotfiles.sh
@@ -6,3 +6,5 @@ DOTFILES_DIR="$( cd ../dotfiles && pwd )"
 
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.aliases" ~
+ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
